e7dd085ff984f6c78c68eb4e9f42e7a45251cf49
Luke Cartledge <luke@lukecartledge.com>
2016-08-23 11:37:47 +0100

    Section 9 - Reading

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class RestaurantsController < ApplicationController
+
   def index
     @restaurants = Restaurant.all
   end
@@ -12,7 +13,12 @@ class RestaurantsController < ApplicationController
     redirect_to '/restaurants'
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   private
+
   def restaurant_params
     params.require(:restaurant).permit(:name)
   end
