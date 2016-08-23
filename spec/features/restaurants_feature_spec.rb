39d1720070cac93ec7d1575eb9369a857be996b1
Zeeshan Ishtiaq <mzishtiaq@yahoo.co.uk>
2016-08-23 16:11:30 +0100

    users can view restaurants

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -5,7 +5,7 @@ feature "restaurants" do
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content('No restaurants yet')
-      expect(page).to have_link 'Add a restaurant'
+      expect(page).to have_link('Add a restaurant')
     end
   end
 
@@ -24,11 +24,22 @@ feature "restaurants" do
   context 'creating restaurants' do
     scenario 'prompts user to fill out a form, then displays the new restaurant' do
       visit '/restaurants'
-      click_link 'Add a restaurant'
+      click_link('Add a restaurant')
       fill_in 'Name', with: 'KFC'
-      click_button 'Create Restaurant'
-      expect(page).to have_content 'KFC'
-      expect(current_path).to eq '/restaurants'
+      click_button('Create Restaurant')
+      expect(page).to have_content('KFC')
+      expect(current_path).to eq('/restaurants')
+    end
+  end
+
+  context 'viewing restaurants' do
+    let!(:kfc) { Restaurant.create(name: 'KFC') }
+
+    scenario "let's users view a restaurant" do
+      visit '/restaurants'
+      click_link 'KFC'
+      expect(page).to have_content('KFC')
+      expect(current_path).to eq("/restaurants/#{kfc.id}")
     end
   end
 end
