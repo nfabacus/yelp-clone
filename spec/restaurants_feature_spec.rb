8dc0d5c6c9a14c2a041f44a760679fa1dcd0fd21
Jose <jcopoviking@outlook.com>
2016-08-23 12:39:10 +0100

    Completes all challenges up to the end of no.7

--- spec/restaurants_feature_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-feature "Viewing restaurants" do
-  scenario "Can vist the home page" do
-    visit '/restaurants'
-    expect(page).to have_content("Rats and Roaches")
-  end
-
-end
